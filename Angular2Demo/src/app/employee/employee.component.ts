384fc09d3f67f464403007ca24ac2b8a22d4f9cd
AnnSamoluk <annsamoluk@mail.ru>
2017-09-26 00:11:28 +0300

    first commit

---
+++ Angular2Demo/src/app/employee/employee.component.ts
@@ -0,0 +1,12 @@
+﻿import { Component } from '@angular/core'
+
+@Component({
+    selector: 'my-employee',
+    templateUrl:'app/employee/employee.component.html'
+})
+export class EmployeeComponent {
+    firstName: string = 'Tom';
+    lastName: string = 'Black';
+    gender: string = 'meil';
+    age:number=20
+}
